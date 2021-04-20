2161b136e54416d7b0cb0540a9ef49c5472895b5
JRomero903 <jromero@tecnologia-colombia.com>
2021-04-20 10:54:41 -0500

    cambia el color del titulo y alterna

---
+++ js/app.js
@@ -0,0 +1,37 @@
+//cambia el color del titulo y alterna
+function colorBlink(selector) {
+	$(selector).animate({
+			opacity: '1',
+		}, {
+			step: function () {
+				$(this).css('color', 'white');
+			},
+			queue: true
+		})
+		.animate({
+			opacity: '1'
+		}, {
+			step: function () {
+				$(this).css('color', 'yellow');
+			},
+			queue: true
+		}, 600)
+		.delay(1000)
+		.animate({
+			opacity: '1'
+		}, {
+			step: function () {
+				$(this).css('color', 'white');
+			},
+			queue: true
+		})
+		.animate({
+			opacity: '1'
+		}, {
+			step: function () {
+				$(this).css('color', 'yellow');
+				colorBlink('h1.main-titulo');
+			},
+			queue: true
+		});
+}
